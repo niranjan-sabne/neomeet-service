ba2fe8376e88f8a192ebabf83aa95e153e49167d
Niranjan <niranjan.sabne@neosoftmail.com>
2022-09-06 21:13:11 +0530

    Fix : Swagger API Tags

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('User')
 @Controller('user')
 export class UserController {
   @UseGuards(AuthGuard('jwt'))
